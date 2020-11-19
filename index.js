d31a03863f0d133d27e5681222938021fc2b14c7
Amartya Sarkar <sarkaramartya0@gmail.com>
2020-11-19 15:39:26 +0530

    final commit

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.use(cors());
 
 var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
+var transporter = nodemailer.createTransport({    //fror link your email and nodemailer
   service: 'gmail',
   secure: true,
   auth: {
-    user: 'amartyasarkark8n0cx06w349338@gmail.com',
-    pass: '@<?#Vivek&*ma349338$>%'
+    user: 'amartyasarkark8n0cx06w349338@gmail.com', //Your Email Address
+    pass: '@<?#Vivek&*ma349338$>%'      //Your Password
   }
   // auth: {
   //   user: 'amartyasarkar4@gmail.com',
@@ -30,7 +30,7 @@ app.post("/sendmail", (req, res) => {
   var clientdata = req.body.clientDetails;
 
   var mailOptions = {
-    to: [clientdata.email,'amartyasarkar0001@gmail.com'],
+    to: ['amartyasarkar0001@gmail.com'],  //give here the main Email where you want to revieve emails of your clients
     from: 'amartyasarkar0001@gmail.com',
     subject: clientdata.name,
     text:` New client Posted a Job For You.Details are here:
@@ -41,20 +41,43 @@ app.post("/sendmail", (req, res) => {
           Classification:${req.body.classification}
           Model:${req.body.model}
           color:${req.body.color}
-          issue:${req.body.issue?req.body.issue:"not defined"}
+          issue:${req.body.issue?req.body.issue:"Undefined"}
+          Extra Inquary:${req.body.inquaryInput}
+          `,
+    phone: clientdata.phone
+  };
+  var mailOptionsClient = {
+    to: [clientdata.email],   //the clients email ,and An email of Successfull Inqury will be send to here
+    from: 'amartyasarkar0001@gmail.com', 
+    subject: clientdata.name,
+    text:`You have Successfully Submitted your Inquary.Details are here:
+          Email: ${clientdata.email}
+          Phone no : ${clientdata.phone}
+          Name : ${clientdata.fullname}
+          Divice Type:${req.body.diviceType}
+          Classification:${req.body.classification}
+          Model:${req.body.model}
+          color:${req.body.color}
+          issue:${req.body.issue?req.body.issue:"Undefined"}
           Extra Inquary:${req.body.inquaryInput}
           `,
     phone: clientdata.phone
   };
   console.log(mailOptions);
-  transporter.sendMail(mailOptions, function (error, info) {
+  transporter.sendMail(mailOptions, function (error, info) { //send to your Email
     if (error) {
       console.log("Error occured");
-      res.json({error:"Check Your Email address"})
+      res.json({error:"Check Your Email address",send:false})
     } else {
+      transporter.sendMail(mailOptionsClient, function (error, info) { //send to clients email
+      if(error){
+        console.log("Error occured in Client mail send");
+        res.json({error:"Check Your Email address",send:false})
+      }else{
+        res.json({message:"your Inquary successfully Posted ,Our Experts will contact with you soon",FullDetails:req.body,send:true})  //this will send to your frontend react app
+      }
       
-      res.json({message:"your Inquary successfully Posted ,Our Experts will contact with you soon",FullDetails:req.body})
-      
+      })
     }
   });
 })
